86300105647bd25e9bcf9fb88fd9c84fb471fda0
Hosanna Otchere <hosannaob2@gmail.com>
2025-12-28 16:25:48 -0500

    fixed everything

--- grade-predictor/src/app/page.tsx
+++ grade-predictor/src/app/page.tsx
@@ -2,85 +2,23 @@ import Image from "next/image";
 'use client';
 import React from "react";
 import { useState } from "react";
+import Link from "next/link";
 import './globals.css'
 
 export default function Home() {
-  const questions = [
-    'I always preview the material that will be discussed before I go to class.',
-    'I go over my lecture notes as soon as possible after lecture to rework them and note problem areas.',
-    'I try to do my homework without using example problems as a guide or copying answers from my class notes or textbook.',
-    'I regularly go to office hours or tutoring to discuss problems or questions about the homework.',
-    'I rework all of the homework problems and questions before the test or quiz.',
-    'I spend some time studying for this class at least five days per week outside of class.',
-    'I make mnemonics for myself to help me remember facts and equations.',
-    'I make diagrams or draw mental pictures of the concepts discussed in class.',
-    'I participate in a study group where we do homework and quiz ourselves on the material.',
-    'I rework all of the quiz and test items I have missed before the next class session.',
-    'I realize that I can still do well in this class even if I have done poorly on the quizzes and tests up to this point.'
-  ];
-  const {currentIndex, setCurrentIndex} = useState(0);
-  const [score, setScore] = useState(0);
-  const [result, setResult] = useState('');
-  const [started, getStarted] = useState(false);
-
-  const displayResult = (score: number) => {
-    if (score <= 2){
-      setResult('F');
-    }
-    else if (score > 2 && score <= 4){
-      setResult('D');
-    }
-    else if (score > 4 && score <= 6){
-      setResult('C');
-    }
-    else if (score > 4 && score <= 8){
-      setResult('B');
-    }
-    else {
-      setResult('A');
-    }
-  }
-
-  const answerQuestion = (response: string) => {
-    if (currentIndex >= questions.length) {
-      if (response === 'yes') {
-        setScore(score + 1);
-      }
-      setCurrentIndex(currentIndex + 1);
-    } else {
-      setResult(displayResult(score + (response === 'yes' ? 1 : 0)));
-
-    }
-    
-  }
-
-  const startQuiz = () => {
-    getStarted(true);
-    setCurrentIndex(0);
-    setScore(0);
-    setResult('');
-  }
-
+  const handleClick = () => {
+    console.log('Start Quiz button clicked');
+  };
   return (
-    <div className="container">
-      <div className="heading">
-          <h1> Grade Predictor</h1>
-          <h2 > Let's Predict Your Grade</h2>
-          { !started ? (
-            <button className="button" onClick={startQuiz}>
-              <span className="button-link"> Predict Now</span>
-            </button>
-          ) :
-          result ? ( 
-            <div> Your predicred grade: {result}</div>
-          ) : (
-            <div>
-              <div>{questions[currentIndex]} </div>
-              <button onClick={() => answerQuestion('yes')} className="button">Yes</button>
-              <button onClick={() => answerQuestion('no')} className="button">No</button>
-            </div>
-          )}
-      </div>
+    <div className="home-container">
+      <h1 className="welcome-header">
+        Let's Predict Your Grade!
+      </h1>
+      <Link href="/quiz">
+        <button className="start-quiz-button" onClick={handleClick}>
+          Start Quiz
+        </button>
+      </Link>
     </div>
   );
 }
---
+++ grade-predictor/src/app/quiz/page.tsx
@@ -0,0 +1,92 @@
+'use client';
+import Link from 'next/link';
+import React, { useState } from 'react';
+export default function QuizPage() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [scoreLetter, setScoreLetter] = useState('');
+  const questions = [
+    'I always preview the material that will be discussed before I go to class.',
+    'I go over my lecture notes as soon as possible after lecture to rework them and note problem areas.',
+    'I try to do my homework without using example problems as a guide or copying answers from my class notes or textbook.',
+    'I regularly go to office hours or tutoring to discuss problems or questions about the homework.',
+    'I rework all of the homework problems and questions before the test or quiz.',
+    'I spend some time studying for this class at least five days per week outside of class.',
+    'I make mnemonics for myself to help me remember facts and equations.',
+    'I make diagrams or draw mental pictures of the concepts discussed in class.',
+    'I participate in a study group where we do homework and quiz ourselves on the material.',
+    'I rework all of the quiz and test items I have missed before the next class session.',
+    'I realize that I can still do well in this class even if I have done poorly on the quizzes and tests up to this point.'
+  ];
+  const [isFinished, setFinished] = useState(false);
+
+  // handle button click 
+  const handleScore = (score: number) => {
+        let letter = '';
+        if (score <= 2){
+            letter = 'F';
+        }
+        else if (score <= 3){
+            letter = 'D';
+        }
+        else if (score <= 5){
+            letter = 'C';
+        }
+        else if (score <= 8){
+            letter = 'B';
+        }
+        else {
+            letter = 'A';
+        }
+        setScoreLetter(letter);
+        console.log('Current Score Letter: ' + letter);
+  };
+    const handleYesClick = () => {
+        console.log('Yes clicked');
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+        setScore(score + 1);
+        if (currentQuestionIndex === questions.length - 1) {
+            setFinished(true);
+            console.log('Quiz finished! Your score is: ' + score);
+        }
+        handleScore(score);
+    };
+    const handleNoClick = () => {
+        console.log('No clicked');
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+        if (currentQuestionIndex === questions.length - 1) {
+            setFinished(true);
+            console.log('Quiz finished! Your score is: ' + score);
+        }
+        handleScore(score);
+    };
+
+    const handleResetClick = () => {
+        setCurrentQuestionIndex(0);
+        setScore(0);
+        setScoreLetter('');
+        setFinished(false);
+    };
+
+    return (
+        <div className='quiz-container'>
+            {!isFinished ? (
+                // Display current question
+                <>
+                    <p className='questions'>{questions[currentQuestionIndex]}</p>
+                    <button className="yes-button" onClick={handleYesClick}>Yes</button>
+                    <button className="no-button" onClick={handleNoClick}>No</button>
+                </>
+            ) : (
+                // Display results
+                <>
+                    <p>Your predicted grade is: {scoreLetter} </p> 
+                    <button className="rest button" onClick={handleResetClick}>Retake Quiz</button>
+                    <Link href="/">
+                        <button className="home-button">Go Home</button>
+                    </Link>
+                </>
+            )}
+        </div>
+    )
+}
