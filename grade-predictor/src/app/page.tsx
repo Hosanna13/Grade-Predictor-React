0009f3dd30187f96e969161a2a20c2deb1e8cf65
Hosanna Otchere <hosannaob2@gmail.com>
2025-12-28 18:47:12 -0500

    to ref later

--- grade-predictor/src/app/page.tsx
+++ grade-predictor/src/app/page.tsx
@@ -13,14 +13,32 @@ export default function Home() {
     <div className="home-container">
     <div className='hero'>
       <h1 className="welcome-header">
-        Let's Predict Your Grade!
+        Let's Predict Your <span className="grade">Grade!</span>
       </h1>
+      <p className="welcome-subheader">
+        Take our quick quiz to find out how your study habits could impact your academic performance.
+      </p>
       <Link href="/quiz">
-        <button className="button" onClick={handleClick}>
-          Start Quiz
+        <button className="start-btn" onClick={handleClick}>
+          Start Quiz 🚀
         </button>
       </Link>
       </div>
+      <br />
+      <div className="benefits-section">
+        <li> <span className="icon">⏱️</span>Takes 5 minutes</li>
+        <li> <span className="icon">🔒</span>100% Anonymous</li>
+        <li> <span className='icon'>📈</span> Access your Habits</li>
+      </div>
+      <footer className="footer">
+        <h1 className="stat">100+ </h1>
+        <p className="stat-desc">Students assessed</p>
+        <h1 className="stat">85% </h1>
+        <p className="stat-desc">Accuracy rate</p>
+        <h1 className="stat"> 12 </h1>
+        <p className="stat-desc">Questions</p>
+      </footer>
+
     </div>
   );
 }
